cc5fecc62660c6f53a5e3a4df51a8fca37014900
naveen1600 <naveengeorgius@gmail.com>
2024-06-26 11:31:54 -0500

    Completed

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,28 @@
+class MissingElement {
+    public static int missElement(int[] nums) {
+        if (nums[0] == 2)
+            return 1;
 
+        int left = 0, right = nums.length - 1, mid;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+
+            if (mid == nums.length - 1 || nums[mid + 1] != nums[mid] + 1)
+                return nums[mid] + 1;
+            else if (mid + 1 == nums[mid])
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        int x = missElement(nums);
+        System.out.println(x);
+    }
+}
